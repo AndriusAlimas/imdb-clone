63542e4579e42d8c20e5ef243d4f03782597d9f2
Andrius Alimas <XandrejoX@yahoo.com>
2023-11-17 14:28:59 +0000

    update sidebar menu with new icons from font awsome

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,13 +1,39 @@
 import React from "react";
 import "./Sidebar.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFilm,
+  faTv,
+  faUser,
+  faListAlt,
+  faStar,
+} from "@fortawesome/free-solid-svg-icons";
 const Sidebar = ({ isActive, onClick }) => {
   return (
     <div className={`sidebar ${isActive ? "active" : ""}`}>
       <button className="close-sidebar" onClick={onClick}>
         X
       </button>
-      <h1>hey</h1>
+      {/* My sIDEBAR MENU */}
+      <menu className="sidebar-menu">
+        <li>
+          <FontAwesomeIcon icon={faFilm} /> <a href="/movies">Movies</a>
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faTv} /> <a href="/tvshows">TV Shows</a>
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faUser} />{" "}
+          <a href="/celebrities">Celebrities</a>
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faListAlt} />{" "}
+          <a href="/watchlist">Watchlist</a>
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faStar} /> <a href="/ratings">Ratings</a>
+        </li>
+      </menu>
     </div>
   );
 };
